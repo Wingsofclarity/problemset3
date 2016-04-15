33e0f7e93f6d412857d271f5cdcfd4a0e052e6b5
Raymond Hammarling <raymond@hammarlings.se>
2016-04-15 11:58:46 +0200

    Implement utils:split/2

--- src/utils.erl
+++ src/utils.erl
@@ -1,16 +1,16 @@
 %% @author Karl Marklund <karl.marklund@it.uu.se>
 
-%% @doc A small collection of utility functions. 
+%% @doc A small collection of utility functions.
 
 
--module(utils). 
+-module(utils).
 
 -export([seqs/1, filter/2, split/2]).
 
 %% To use EUnit we must include this.
 -include_lib("eunit/include/eunit.hrl").
 
--compile(export_all). 
+-compile(export_all).
 
 
 %% @doc Generates a list of lists of increasing sequences of integers
@@ -28,27 +28,27 @@ seqs(N) ->
     %% since each sequence is generated from scratch. Hence, lets
     %% re-use the last sequnece and add a new element when
     %% constructing the next sequence.
-    
+
     F = fun(X,[H|T]) -> [[X|H],H|T] end,
     lists:foldl(F, [[]], lists:seq(1,N)),
     lists:reverse([lists:reverse(L) || L <- lists:foldl(F, [[]], lists:seq(1,N))]).
 
-		
+
 %% @doc Each list in List2 contains the elements Elem in List1 for
 %% which one of the Pred(Elem) returns true. The order of the lists in
 %% List2 is the same as the order of the predicates. In each list in
 %% List2, the relative order of the elements are the same as in the
-%% original List1. 
-%% 
+%% original List1.
+%%
 %% === Example ===
 %% <div class="example">```
 %% 1> L = [1,2,3,4,5,6,7,8,9,10].
 %% [1,2,3,4,5,6,7,8,9,10]
 %% 2> P1 = fun(X) -> X rem 2 == 1 end.
-%% #Fun<erl_eval.6.111823515>  
-%% 3> P2 = fun(X) -> not P1(X) end. 
 %% #Fun<erl_eval.6.111823515>
-%% 4> P3 = fun(X) -> X > 3 andalso X < 7 end. 
+%% 3> P2 = fun(X) -> not P1(X) end.
+%% #Fun<erl_eval.6.111823515>
+%% 4> P3 = fun(X) -> X > 3 andalso X < 7 end.
 %% #Fun<erl_eval.6.111823515>
 %% 5> utils:filter([P1,P2,P3], L).
 %% [[1,3,5,7,9],[2,4,6,8,10],[4,5,6]]'''
@@ -64,7 +64,7 @@ filter(Predicates, List) ->
     Collect = self(),
     [spawn(fun() -> Collect!{I,lists:filter(P,List)} end) ||
 	{I, P} <- lists:zip(lists:seq(1, length(Predicates)), Predicates)],
-    
+
     filter_collect(length(Predicates), []).
 
 filter_collect(0,R) ->
@@ -80,29 +80,29 @@ lqr(L, N) ->
     Len = length(L),
 
     %% Quotient
-    Q = Len div N, 
-    
-    %% Reminder
-    R = Len rem N, 
-    
-    {Len, Q, R}. 
+    Q = Len div N,
 
-%% @doc Split List into N Lists such that all Lists have approximately the same number of elements. 
-%% 
-%% Let Len = length(List), Q = Len div N and R = Len rem N. 
-%% 
-%% If R = 0, then all of the lists in Lists will be of length Q. 
-%% 
+    %% Reminder
+    R = Len rem N,
+
+    {Len, Q, R}.
+
+%% @doc Split List into N Lists such that all Lists have approximately the same number of elements.
+%%
+%% Let Len = length(List), Q = Len div N and R = Len rem N.
+%%
+%% If R = 0, then all of the lists in Lists will be of length Q.
+%%
 %% If R =/= 0, then R of the lists in Lists will have
-%% lenght Q + 1. 
-%% 
+%% lenght Q + 1.
+%%
 %% === Example ===
-%% 
+%%
 %% <div class="example">```
 %% 1> L = [1,2,3,4,5,6,7,8,9,10].
 %% [1,2,3,4,5,6,7,8,9,10]
 %% 2> utils:split(L, 4).
-%% [[1,2],[3,4],[5,6,7],[8,9,10]]
+%% [[1,2,3],[4,5,6],[7,8],[9,10]]
 %% 3> lists:concat(utils:split(L,3)).
 %% [1,2,3,4,5,6,7,8,9,10]'''
 %% </div>
@@ -112,11 +112,20 @@ lqr(L, N) ->
       T :: term(),
       N :: integer().
 
+split([], _) ->
+    [];
 
 split(L, N) ->
-    tbi.
+    Len = length(L),
+    if
+        Len rem N > 0 ->
+            SplitLen = (Len div N) + 1;
 
-
+        true ->
+            SplitLen = Len div N
+    end,
+    {L1, L2} = lists:split(SplitLen, L),
+    [L1 | split(L2, N-1)].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                                                                          %%
@@ -132,8 +141,8 @@ seqs_length_test_() ->
 
 seqs_test_() ->
     %% A small collection of expected results {N, seqs(N)}.
-    
-    Data = [{0, [[]]}, {1, [[], [1]]}, {2, [[], [1], [1,2]]}, 
+
+    Data = [{0, [[]]}, {1, [[], [1]]}, {2, [[], [1], [1,2]]},
 	    {7, [[],
 		 [1],
 		 [1,2],
@@ -143,21 +152,21 @@ seqs_test_() ->
 		 [1,2,3,4,5,6],
 		 [1,2,3,4,5,6,7]]}
 	   ],
-    
+
     [?_assertEqual(L, seqs(N)) || {N, L} <- Data].
-    
+
 filter_test_() ->
     [?_assertEqual([], filter([], L)) || L <- seqs(10)].
-    
+
 filter_true_false_test_() ->
     P1 = fun(_) -> false end,
     P2 = fun(_) -> true end,
     P3 = fun(X) -> X rem 2 == 0 end,
-    
+
     Expected = fun(L) -> [lists:filter(P,L) || P <- [P1,P2,P3]] end,
 
     [?_assertEqual(Expected(L), filter([P1,P2,P3], L) ) || L <- seqs(10) ].
-				       
+
 filter_test() ->
     L = lists:seq(1,10),
 
@@ -167,19 +176,19 @@ filter_test() ->
 
     %%E = [[2,4,6,8,10],[1,3,5,7,9],[4,5,6,7,8,9,10]],
     E = [lists:filter(P,L) || P <- [P1,P2,P3]],
-    
+
     ?assertEqual(E, filter([P1,P2,P3], L)).
-    
+
 split_concat_test_() ->
     %% Make sure the result of concatenating the sublists equals the
     %% original list.
-    
+
     L = lists:seq(1,99),
     [?_assertEqual(L, lists:concat(split(L,N))) || N <- lists:seq(1,133)].
 
 split_n_test_() ->
-    %% Make sure the correct number of sublists are generated. 
-    
+    %% Make sure the correct number of sublists are generated.
+
     M = 99,
     L = lists:seq(1,M),
     Num_of_lists = fun(List, N) when N =< length(List) ->
@@ -187,20 +196,20 @@ split_n_test_() ->
 		      (List, _) ->
 			   length(List)
 		   end,
-    [?_assertEqual(Num_of_lists(L,N), length(split(L,N))) || N <- L].    
+    [?_assertEqual(Num_of_lists(L,N), length(split(L,N))) || N <- L].
 
 
 expected_stat(L, N) when N =< length(L) ->
     %% When spliting a list L into N sublists, we know there will only by two possible
     %% lengths of the sublists.
 
-    
-    %% Quotient and reminder when dividing length of L with N. 
+
+    %% Quotient and reminder when dividing length of L with N.
     {_, Q, R} = lqr(L, N),
 
     %% There will allways be R sublists of length Q+1 and N-R sublists
     %% of length Q.
-    
+
     {{R, Q+1}, {N-R, Q}};
 
 expected_stat(L, _N) ->
@@ -213,24 +222,23 @@ stat(N, M, LL) ->
     %% Return a tuple {{Num_N, N}, {Num_M, M}} where Num_N is the
     %% number of lists of length N in LL and Num_M is the number of
     %% lists of length M in LL.
-    
+
     S = filter([fun(X) -> X == N end, fun(X) -> X == M end], [length(L) || L <- LL]),
 
     [Num_N, Num_M] = [length(L) || L <- S],
-    
+
     {{Num_N, N}, {Num_M, M}}.
 
 split_stat_test_() ->
     %% Assure the list of sublists contains the correct number of
     %% lists of the two expected lengths.
-	
+
     Assert = fun(L,N) ->
-		     {_, Q, _} = lqr(L,N), 
-		     ?_assertEqual(expected_stat(L,N), stat(Q+1, Q, split(L,N))) 
+		     {_, Q, _} = lqr(L,N),
+		     ?_assertEqual(expected_stat(L,N), stat(Q+1, Q, split(L,N)))
 	     end,
-	
+
     %% Generators can depend on other generator expressions, here N
     %% depends on the length of L.
-    
+
     [Assert(L,N) ||  L <- seqs(33), N <- lists:seq(1,length(L)+5)].
-    
