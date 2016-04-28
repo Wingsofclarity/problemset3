a44863621198243b20979d46def054794182c01a
Raymond Hammarling <raymond@hammarlings.se>
2016-04-28 15:48:32 +0100

    Add speculative execution

--- src/add.erl
+++ src/add.erl
@@ -31,7 +31,13 @@ start(A, B, Base, Options) ->
     ),
     StartChild ! {carry, 0},
     {Result, CarryOut} = receive_results([]),
-    lists:append([integer_to_list(CarryOut) | Result]).
+    if
+        CarryOut =:= 1 ->
+            lists:append([integer_to_list(CarryOut) | Result]);
+
+        CarryOut =:= 0 ->
+            lists:append(Result)
+    end.
 
 %% @doc TODO: add documentation
 -spec start_child(A,B,NextPid,ParentPid,Base) -> pid() when
@@ -43,15 +49,36 @@ start(A, B, Base, Options) ->
 
 start_child(A, B, NextPid, ParentPid, Base) ->
     spawn(fun() ->
+        Children = [
+            start_speculative_child(A, B, Base, self(), 0),
+            start_speculative_child(A, B, Base, self(), 1)
+        ],
+
         receive
             {carry, CarryIn} ->
+                CorrectCarryPid = lists:nth(CarryIn+1, Children),
+                WrongCarryPid = lists:nth(2-CarryIn, Children),
                 % io:format("~w; A: ~s; B: ~s; Carry in: ~w~n", [self(), A, B, CarryIn]),
 
-                {Result, CarryOut} = utils:sum(A, B, CarryIn, Base),
+                exit(WrongCarryPid, kill),
+                CorrectCarryPid ! life,
 
                 % io:format("~w; RESULT: ~s; CARRY OUT: ~w~n", [self(), Result, CarryOut]),
-                ParentPid ! {result, Result},
-                NextPid ! {carry, CarryOut}
+                receive
+                    {result, {Result, CarryOut}} ->
+                        ParentPid ! {result, Result},
+                        NextPid ! {carry, CarryOut}
+                end
+        end
+    end).
+
+start_speculative_child(A, B, Base, ParentPid, CarryInEst) ->
+    spawn(fun() ->
+        Result = utils:sum(A, B, CarryInEst, Base),
+
+        receive
+            life ->
+                ParentPid ! {result, Result}
         end
     end).
 
